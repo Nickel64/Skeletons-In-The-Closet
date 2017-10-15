d72cb765c4c455a1945bb62be1d7236afb5ae4b4
Ben Percy <thesvenskaanka@gmail.com>
2017-10-15 21:27:01 +1300

    Tidying

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -4,13 +4,37 @@ package Entities;
  * Created by Shlomoburg on 19/09/2017.
  */
 public interface Entity {
+
+
     enum Direction{Up,Down,Left,Right}
-     boolean inAggroRange();
+
+    /**
+     *
+     * @return check to see whether the entity is dead or not
+     */
      boolean isDead();
+
+    /**
+     * @param entity, the entity to be attacked
+     * the entity attacks the entity passed in through the parameters
+     */
      void attack(Entity entity);
+
+    /**
+     * decreases the entities health, and then calls isdead, checks whether the entity has died
+     */
      void damaged(int damage);
+
+    /**
+     * the level of the entity
+     */
      void setDirection(Direction dir);
-     int getLevel();
+
+    /**
+     *
+     * @return the level of the entity
+     */
+    int getLevel();
 
     /**
      * current entity attack
@@ -44,13 +68,11 @@ public interface Entity {
      */
     boolean canStepOn();
 
-    //the boolean determines if an action is complete (AoE, attack,dying etc)
+    /**
+     * the boolean determines if an action is complete (AoE, attack,dying etc)
+     * @return whether the action completes or not
+     */
     boolean ping();
 
-   // damaged
-   // turn
-    //move
-   // chooseaction
-    //attack
 
 }
