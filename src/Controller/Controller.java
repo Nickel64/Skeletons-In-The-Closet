aad6847c1daf02eaa0253a802dca7a3c46eccc84
Rachel Anderson <andersrach8@ecs.vuw.ac.nz>
2017-10-16 19:48:41 +1300

    Shorter bgm now!

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -37,7 +37,7 @@ public class Controller implements KeyListener, MouseListener, ActionListener {
 
     private void startLoop() {
         new Timer(50, (e) -> {
-            if((bgmIteration++ % 3880) == 0) { //bgm is 194s * 20 pings/sec
+            if((bgmIteration++ % 800) == 0) { //bgm is 40s * 20 pings/sec
                 bgmIteration = 1;
                 Resources.bgm.start();
             }
